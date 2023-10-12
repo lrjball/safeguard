134515a0c9bf2f5d072401403388121aac754dac
lrjball <lrjball@gmail.com>
2023-10-12 11:31:49 +0100

    Added data

--- guard/views.py
+++ guard/views.py
@@ -10,7 +10,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Therapist, Appointment, Client, SafeguardLead
 from django.utils import timezone
-
+from random import random
 
 @login_required
 def index(request):
@@ -21,10 +21,27 @@ def index(request):
     )
 
 
+IMAGE_URLS = [i for j in range(100) for i in  [
+    "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+]
+]
+
+
 class TherapistListView(ListView):
     model = Therapist
     paginate_by = 100
     template_name="guard/lead_list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['urls'] = IMAGE_URLS
+        context['therapist_url_list'] = zip(context['therapist_list'] ,IMAGE_URLS)
+        return context
 # TODO: add map!
 #     def get_context_data(self, **kwargs):
 #         context = super().get_context_data(**kwargs)
@@ -68,12 +85,34 @@ class TherapistDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['appointments'] = context['therapist'].appointment_set.exclude(checked_out=True).order_by('start_time')
+        context['urls'] = IMAGE_URLS
         return context
 
 
 class AppointmentDetailView(DetailView):
     model = Appointment
     template_name="guard/appointment.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url'] = IMAGE_URLS[0]
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        figure = folium.Figure()
+        m = folium.Map(
+            location=[53.27162, -2.34790],
+            zoom_start=15,
+            tiles='openstreetmap'
+        )
+        m.add_to(figure)
+        folium.Marker(
+            location=[53.27162, -2.34790],
+            popup= """Meeting Location""",
+            icon=folium.Icon(color='blue')
+        ).add_to(m)
+        m = m._repr_html_()
+        context['map'] = m
+        return context
 
 
 def appointment_check_in(request, pk):
